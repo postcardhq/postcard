3280b34f06281200b7cb7c2e20f5e66d6fea2575
yves <yves@Mac-56.lan>
2026-04-03 20:30:08 -0700

    Added basic web skeleton (clean version)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { EvidenceUpload } from "@/components/ui/UploadFileBox";
 
-export default function Home() {
+export default function PostcardHome() {
   return (
-    <h1>Postcard</h1>
+    <div className="flex h-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-2xl font-bold"></h1>
+      <EvidenceUpload />
+    </div>
   );
 }
